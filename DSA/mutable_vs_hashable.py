d2fa6144f2e6187b73aded6699cfa82099f0c821
Kadir Emir <92856123+kadiremir@users.noreply.github.com>
2025-01-19 13:57:08 +0000

    edit

---
+++ DSA/mutable_vs_hashable.py
@@ -0,0 +1,22 @@
+"""2025, Kadir Emir, https://github.com/kadiremir
+
+In this file, we explain the relationship between hashable and mutable data types in Python.
+
+The following table shows which data types are hashable and which are mutable:
+
++---------------------+---------------------+
+| Hashable (Immutable)| Non-Hashable (Mutable) |
++---------------------+---------------------+
+| Strings             | Lists/Arrays         |
+| Integers            | Dictionaries         |
+| Tuples              | Sets                |
++---------------------+---------------------+
+
+Hashable data types are immutable and can be used as keys in dictionaries or elements in sets.
+Because they are immutable, their hash value does not change, which makes them suitable for use in hash tables.
+
+Non-hashable data types are mutable and cannot be used as keys in dictionaries or elements in sets.
+Because they are mutable, their hash value can change, which makes them unsuitable for use in hash tables.
+
+Note that this convention and scheme is Python-specific and may vary in other programming languages.
+"""
