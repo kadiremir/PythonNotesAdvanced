40fc562f8d880a582da952238e30ee6ca3997219
Kadir Emir <92856123+kadiremir@users.noreply.github.com>
2025-01-19 14:22:10 +0000

    edit

---
+++ DSA/comparison.py
@@ -0,0 +1,12 @@
+"""
+Data Structure Comparison
+|---------------------------------------------------------------------------------------------------------|
+| Data Structure | Storage Mechanism       | Hashing Used | Membership Check Complexity | Maintains Order |
+|----------------|-------------------------|--------------|-----------------------------|-----------------|
+| List           | Dynamic Array           | No           | O(n)                        | Yes             |
+| Array          | Typed Dynamic Array     | No           | O(n)                        | Yes             |
+| Set            | Hash Table              | Yes          | O(1) (avg), O(n) (worst)    | No              |
+| Dictionary     | Hash Table              | Yes          | O(1) (avg), O(n) (worst)    | Yes             |
+|---------------------------------------------------------------------------------------------------------|
+
+"""
