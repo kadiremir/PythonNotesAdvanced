0ec75d8b410daf7b1bc912872210e25b4d686b9d
Kadir Emir <92856123+kadiremir@users.noreply.github.com>
2025-01-19 14:09:03 +0000

    edit

--- DSA/mutable_vs_hashable.py
+++ DSA/mutable_vs_hashable.py
@@ -19,4 +19,17 @@ Non-hashable data types are mutable and cannot be used as keys in dictionaries o
 Because they are mutable, their hash value can change, which makes them unsuitable for use in hash tables.
 
 Note that this convention and scheme is Python-specific and may vary in other programming languages.
+
+####################################################################################################
+
+Lets see what happens when we check an item is in a list or a set.
+Consider we have a list of 1000 items and a set of 1000 items.
+
+
+    For the list, we have to check each item one by one.
+    So the time complexity is O(n).
+
+    For the set, we can use a hash table to check if the item is in the set.
+    The time complexity is O(1) on average.
+    This is because hash tables have constant time complexity for lookups, insertions, and deletions.
 """
