fc717e8f3e9ba6302825ea736334b232307fa59f
kadir emir <kadir.emir@thermofisher.com>
2025-01-21 09:26:33 +0100

    edit

---
+++ GENERIC ADVANCED TOPICS/is_vs_equal.py
@@ -0,0 +1,30 @@
+"""2025, Kadir Emir, https://github.com/kadiremir
+
+This file is to explain the difference between "is" and "==" operators in Python.
+
+is:
+    - Compares the memory address of the objects.
+    - Returns True if both objects are the same object.
+
+==:
+    - Compares the values of the objects.
+    - Returns True if the values of the objects are equal.
+"""
+
+#############################################
+# Example 1
+#############################################
+a = [1, 2, 3]
+b = a
+c = [1, 2, 3]
+
+print(a is b)  # True, because `b` references the same object as `a`.
+print(a is c)  # False, because `c` is a different object with the same content.
+
+#############################################
+# Example 2
+#############################################
+a = [1, 2, 3]
+c = [1, 2, 3]
+
+print(a == c)  # True, because the contents of `a` and `c` are the same.
