804bfcacd81a180c02f558b05c062c33b238836f
kadir emir <kadir.emir@thermofisher.com>
2025-01-21 09:43:09 +0100

    edit

---
+++ DSA/named_tuple.py
@@ -0,0 +1,27 @@
+"""2025, Kadir Emir, https://github.com/kadiremir
+
+This file is to explain Named Tuples in Python.
+
+Named Tuples:
+    - Are extensions of the regular tuples.
+    - Attributes can be accessed by name.
+    - Created via a factory function.
+    - Factory metthod takes two arguments:
+        - class name
+        - list of field names.
+
+    - They are immutable, like regular tuples.
+"""
+
+from collections import namedtuple
+
+# Creating a Named Tuple
+Color = namedtuple('Color', ('red', 'green', 'blue'))
+
+# Creating an instance of the Named Tuple
+color = Color(255, 0, 0)
+
+# Accessing the fields by name
+print(color.red)  # 255
+print(color.green)  # 0
+print(color.blue)  # 0
